e493196f88b095910d47b1da226c3ec1ebc9e58c
TheEpicBlock <git.teb@theepicblock.nl>
2024-02-29 19:25:45 +0100

    Double addition test
    because the result of instructions appear to have names in llvm, I wasn't sure if they'd conflict

---
+++ testsuite/tests/DoubleAddition.rs
@@ -0,0 +1,8 @@
+use vm_core::ClassLoader;
+use vm_core::VirtualMachine;
+use vm_llvm::LlvmJitCompiler;
+
+fn run(classpath: impl ClassLoader) {
+    let mut vm = VirtualMachine::new(classpath, LlvmJitCompiler::default());
+    vm.run("DoubleAddition", "add", "()I").unwrap();
+}
